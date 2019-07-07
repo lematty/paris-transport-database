b416a515f114beb45843236d602cc0c19e50593b
Cameron Jones <camandrewjones@gmail.com>
2019-07-07 23:58:08 +0200

    improved createdb speed x5

--- createdb.bash
+++ createdb.bash
@@ -1,28 +1,56 @@
 #!/bin/bash
 
 zippath="./source/buses/"
+
+ziplist=($(ls ${zippath}*.zip))
+
 mkdir -p dbinit
-mkdir -p ziptmp
+
+function work() {
+    for f; do [[ -f $f ]] && process_zip "$f"; done
+}
+
+function process_zip() {
+    name="${1##*/}"
+    tmpdir="${name}_tmp"
+    seen=$(ls dbinit | grep "$name")
+    if [[ ${seen} != '' ]]; then
+        continue
+    fi
+
+    mkdir -p ${tmpdir}
+    unzip ${1} -d ${tmpdir}
+
+    for t in ${tmpdir}/*.txt; do
+        python ./csv2postgres.py ./${tmpdir} ${name}
+    done
+     rm -rf ${tmpdir}
+}
 
 if [[ "$1" == "schema" ]]; then
     python ./schema.py && mv 0_schema.sql dbinit
     exit 0
 fi
 
-for z in ${zippath}/*.zip; do
-    name="${z##*/}"
-    seen=$(ls dbinit | grep "$name")
-    if [[ ${seen} != '' ]]; then
-        continue
-    fi
-
-    rm -f ziptmp/*
-    unzip ${z} -d ziptmp
-
-    for t in ziptmp/*.txt; do
-        echo ${t}
-        python ./csv2postgres.py ./ziptmp ${name} && mv *.sql dbinit
-    done
+# Divide the list into 5 sub-lists.
+i=0 n=0 a=() b=() c=() d=() e=()
+while ((i < ${#ziplist[*]})); do
+    echo ${ziplist[i]}
+    a[n]=${ziplist[i]}
+    b[n]=${ziplist[i+1]}
+    c[n]=${ziplist[i+2]}
+    d[n]=${ziplist[i+3]}
+    e[n]=${ziplist[i+4]}
+    ((i+=5, n++))
 done
 
-rm -rf ziptmp
+
+# Process the sub-lists in parallel
+work "${a[@]}" &
+work "${b[@]}" &
+work "${c[@]}" &
+work "${d[@]}" &
+work "${e[@]}" &
+wait
+
+mv *.sql dbinit
